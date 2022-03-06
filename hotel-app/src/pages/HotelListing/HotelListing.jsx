ce4a0b3bc2c4956e7e1804441670989a6a36c196
yznnajjar <99915606+yznnajjar@users.noreply.github.com>
2022-03-06 19:16:54 +0300

    Added Pages Folder

---
+++ hotel-app/src/pages/HotelListing/HotelListing.jsx
@@ -0,0 +1,179 @@
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import PropTypes from 'prop-types';
+//Import Helpers
+import Layout from '../../components/Layout';
+import CardDate from "../../components/CardDate";
+import useDebounce from "../../lib/hooks/useDebounce";
+import {
+  HOTEL_LISTING,
+  PRICE_FILTER,
+  SORT_BY_NAME,
+  SORT_BY_PRICE,
+  TOTAL_NIGHTS,
+  SEARCH_PLACEHOLDER,
+} from '../../lib/helpers/constant'
+//Import Style
+import './HotelListing.scss';
+
+const HotelListing = (props) => {
+  const [hotelList, setHotelList]             = useState([]);
+  const [searchHotelName, setSearchHotelName] = useState("");
+  const [priceValue, setPriceValue]           = useState(0)
+  
+  const dSearchText  = useDebounce(searchHotelName, 500);
+  const dbPriceValue = useDebounce(priceValue, 1000);
+
+  //Mount Data For Hotel Listing
+  useEffect(()=>{
+    setHotelList(props.data);
+  },[]);
+
+  // onSearch called only when debounced search text changes
+  useEffect(() => {
+    if (!dSearchText) {
+      setSearchHotelName("");
+      setHotelList(props.data)
+    }
+
+
+    if (dSearchText && props.data.length) {
+      const filteredData = props.data.filter(item => item.name.includes(dSearchText))
+      setHotelList(filteredData);
+    }
+  }, [dSearchText]);
+
+  // //Effect When Price Filter Change 
+  useEffect(()=>{
+    if (!dbPriceValue) {
+      setPriceValue(0);
+      setHotelList(props.data)
+    }
+
+
+    if (dbPriceValue && props.data.length) {
+      const isTherePrice = props.data.filter(item => +item.price < +dbPriceValue) || [];
+      setHotelList([...isTherePrice])
+    }
+  },[dbPriceValue])
+
+  useEffect(() => {
+    if(props.data.length === 0) return;
+    setHotelList(props.data);
+  }, [props.data.length]);
+
+  const handleSortByName = useCallback(() => {
+    if(hotelList.length === 1) return;
+    
+    const sortHotelListByName = hotelList.sort((a, b) => a.name.localeCompare(b.name));
+    setHotelList([...sortHotelListByName])
+  }, [hotelList, dbPriceValue]);
+
+  const handleSortByPrice = useCallback(() => {
+    //No Need To Call Sort If Array Has One Element
+    if(hotelList.length === 1) return;
+
+    const sortHotelListByPrice = hotelList.sort((a, b) => a.price - b.price);
+    setHotelList([...sortHotelListByPrice])
+  }, [hotelList, dbPriceValue]);
+
+  const showSortFilterAndTotalNights = useMemo(() => {
+    return (
+      <div className="card-container__header">
+        <span className="total--nights">{TOTAL_NIGHTS}: {hotelList.length}</span>
+        <div className="sort--filter">
+          <span onClick={ handleSortByName }>
+            { SORT_BY_NAME }
+          </span>
+          <span
+            onClick={ handleSortByPrice }
+          >
+            { SORT_BY_PRICE }
+          </span>
+        </div>
+      </div>
+    )
+  }, [hotelList, dbPriceValue]);
+
+  const renderCards = useMemo(() => {
+    if (hotelList.length === 0) return;
+
+    return (
+      <div className="card-container__cards">
+        {hotelList.map((card, index) => (
+          <CardDate
+            key={ index }
+            name={ card.name }
+            price={ card.price }
+            city={ card.city }
+          />
+        ))}
+      </div>
+    )
+  }, [hotelList, dbPriceValue]);
+
+  const showHotelAppointmentCards = useMemo(() => {
+    return (
+      <div className="card-container">
+        {/* Show Sort Filter With Total Nights */ }
+        { showSortFilterAndTotalNights }
+
+        { renderCards }
+      </div>
+    )
+  }, [hotelList, searchHotelName]);
+
+
+  const showSideFilter = useCallback(() => {
+    return (
+      <div className="hotel-listing__side-filter">
+        <input
+          type="search"
+          placeholder={SEARCH_PLACEHOLDER}
+          className="search__hotel-name"
+          onChange={ (e) => {
+            setSearchHotelName(e.target.value)
+          } }
+          value={ searchHotelName }
+        />
+
+        <div className="range--filter">
+          <span className="range--filter__label"> { PRICE_FILTER }</span>
+          <input
+            type="range"
+            className="range--filter__action"
+            min="0"
+            step="25"
+            value={priceValue}
+            max={"1000"}
+            onInput={event=>{
+              setPriceValue(event.target.value)
+            }}
+          />
+          <p>{priceValue}</p>
+        </div>
+      </div>
+    )
+  }, [searchHotelName, priceValue, hotelList]);
+
+
+  return (
+    <div className="hotel-listing__container">
+      <Layout title={ HOTEL_LISTING }>
+        <div className="hotel-listing__content">
+          {/*Show Side Filter*/ }
+          { showSideFilter() }
+          { showHotelAppointmentCards }
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+HotelListing.propTypes = {
+  data: PropTypes.array.isRequired
+};
+
+HotelListing.defaultProps = {
+  data : []
+}
+export default HotelListing;
---
+++ hotel-app/src/pages/HotelListing/HotelListing.test.jsx
@@ -0,0 +1,166 @@
+import React from 'react'
+import {render, fireEvent, act, screen} from '@testing-library/react'
+import HotelListing from './HotelListing';
+import {
+  HOTEL_LISTING, 
+  PRICE_FILTER, 
+  SORT_BY_NAME, 
+  SORT_BY_PRICE, 
+  TOTAL_NIGHTS, 
+  SEARCH_PLACEHOLDER, 
+  CARD_LABEL, 
+  CURRENCY
+} from '../../lib/helpers/constant';
+
+const dataTest = [
+  {
+    "name": "Kempinski Hotel Mall of the Emirates",
+    "price": "200",
+    "city": "dubai",
+    "available_on": "2022-10-21"
+  },
+  {
+    "name": "Address Dubai Mall",
+    "price": "250",
+    "city": "dubai",
+    "available_on": "2022-08-15"
+  },
+  {
+    "name": "JW Marriott Marquis Hotel Dubai",
+    "price": "225",
+    "city": "dubai",
+    "available_on": "2023-09-21"
+  },
+  {
+    "name": "Hilton Dubai Al Habtoor City",
+    "price": "275",
+    "city": "dubai",
+    "available_on": "2022-10-25"
+  },
+  {
+    "name": "Sofitel Dubai Downtown",
+    "price": "300",
+    "city": "dubai",
+    "available_on": "2022-09-20"
+  },
+  {
+    "name": "Renaissance Downtown Hotel",
+    "price": "200",
+    "city": "dubai",
+    "available_on": "2022-10-23"
+  },
+  {
+    "name": "Sofitel Dubai Jumeirah Beach",
+    "price": "250",
+    "city": "dubai",
+    "available_on": "2022-09-20"
+  },
+  {
+    "name": "Ramada Downtown Dubai",
+    "price": "225",
+    "city": "dubai",
+    "available_on": "2022-09-25"
+  },
+  {
+    "name": "Sheraton Mall of the Emirates Hotel",
+    "price": "325",
+    "city": "dubai",
+    "available_on": "2022-10-24"
+  },
+  {
+    "name": "Emirates Grand Hotel Apartments",
+    "price": "300",
+    "city": "dubai",
+    "available_on": "2022-10-27"
+  },
+]
+
+describe("Test Hotel Listing",()=>{
+  test("Test Case For Labels And Titles",()=>{
+    render(
+      <HotelListing
+        data={dataTest}
+      />
+    );
+
+    screen.getByText(HOTEL_LISTING)
+    screen.getByText(PRICE_FILTER)
+    screen.getByText(SORT_BY_NAME)
+    screen.getByText(SORT_BY_PRICE)
+    screen.getByText(`${TOTAL_NIGHTS}: ${dataTest.length}`);
+  })
+
+  test("Test Case For Cards Name And Price And Label (Text In Each Card)",()=>{
+    render(
+      <HotelListing 
+        data={dataTest}
+      />
+    );
+
+    //Iterating Over Card And Check Name And City And Price
+    dataTest.forEach((info,index)=>{
+      expect(screen.getAllByTestId("name")[index].textContent).toBe(`${CARD_LABEL.HOTEL_NAME}: ${info.name}`)
+      expect(screen.getAllByTestId("price")[index].textContent).toBe(`${CARD_LABEL.HOTEL_PRICE} : ${info.price} ${CURRENCY.UAE_CURRENCY}`)
+      expect(screen.getAllByTestId("city")[index].textContent).toBe(`${CARD_LABEL.HOTEL_City} : ${info.city}`)
+    })
+  });
+
+  test("Test Case For Sort Filter (Sort By Name)≈", async ()=>{
+    render(
+      <HotelListing
+        data={dataTest}
+      />
+    );
+
+    const sortByNameButton = screen.getByText("Sort By Name");
+
+    const afterArrSorted = dataTest.sort((a, b) => a.name.localeCompare(b.name));
+    await act(async()=>fireEvent.click(sortByNameButton));
+
+    //Iterating Over Card Sorted By Name And Check Name And City And Price
+    afterArrSorted.forEach((info,index)=>{
+      expect(screen.getAllByTestId("name")[index].textContent).toBe(`${CARD_LABEL.HOTEL_NAME}: ${info.name}`)
+      expect(screen.getAllByTestId("price")[index].textContent).toBe(`${CARD_LABEL.HOTEL_PRICE} : ${info.price} ${CURRENCY.UAE_CURRENCY}`)
+      expect(screen.getAllByTestId("city")[index].textContent).toBe(`${CARD_LABEL.HOTEL_City} : ${info.city}`)
+    })
+  });
+
+  test("Test Case For Sort Filter (Sort By Price)≈", async ()=>{
+    render(
+      <HotelListing 
+        data={dataTest}
+      />
+    );
+
+    const sortByNameButton = screen.getByText("Sort By Price");
+
+    const afterArrSorted = dataTest.sort((a, b) => a.price - b.price);
+    await act(async()=>fireEvent.click(sortByNameButton));
+
+    //Iterating Over Card Sorted By Name And Check Name And City And Price
+    afterArrSorted.forEach((info,index)=>{
+      expect(screen.getAllByTestId("name")[index].textContent).toBe(`${CARD_LABEL.HOTEL_NAME}: ${info.name}`)
+      expect(screen.getAllByTestId("price")[index].textContent).toBe(`${CARD_LABEL.HOTEL_PRICE} : ${info.price} ${CURRENCY.UAE_CURRENCY}`)
+      expect(screen.getAllByTestId("city")[index].textContent).toBe(`${CARD_LABEL.HOTEL_City} : ${info.city}`)
+    })
+  });
+
+  test("Test Case When Search For Hotel Name",async ()=>{
+    render(
+      <HotelListing
+        data={dataTest}
+      />
+    );
+
+    const searchField = screen.getByRole("searchbox");
+
+    expect(searchField.getAttribute("placeholder")).toBe(SEARCH_PLACEHOLDER)
+
+    // change input
+    await act(async ()=>{
+      fireEvent.change(searchField, {target: {value: "Dubai"}});
+    });
+
+    expect(searchField.getAttribute("value")).toBe("Dubai");
+  });
+})
